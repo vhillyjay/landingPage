bcfacb9833b682ac320ff916c3c0da27e6af77bf
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-07-15 16:16:28 +0800

    our products and map content

---
+++ map.js
@@ -0,0 +1,12 @@
+let map = L.map('map').setView([13.7565, 121.0583], 12);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+
+let marker = L.marker([13.7731, 121.0484]).addTo(map);
